2bb66b3cfed82832e5daa466ee474739f9012b2e
Kieran Collins <kiecol@protonmail.com>
2023-02-09 19:24:11 +0000

    Add gamplay tests and lightsOn function

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,5 @@
+// const { doc } = require("prettier");
+
 let game = {
     score: 0,
     currentGame: [],
@@ -10,29 +12,47 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
+};
+
+function addTurn() {
+    /**
+     * Clear playerMoves array
+     * Randomly add a button ID to currentGame array
+     * Call showTurns function
+     */
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[Math.floor(Math.random() * game.choices.length++)]); // Note: replaced 4 with game.choices.length++
+    // showTurns();
 };
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
-// function addTurn() {
-
-// };
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+};
 
 // function showTurns() {
 
 // };
 
-// function lightsOn() {
-    
-// };
+
 
 // function playerTurn() {
-    
+
 // };
 
 
 
-module.exports = { game, newGame, showScore };
-
+module.exports = {
+    game,
+    newGame,
+    showScore,
+    addTurn,
+    lightsOn
+};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,12 +2,7 @@
  * @jest-environment jsdom
  */
 
-// const { expect } = require("@jest/globals");
-// const { default: test } = require("node:test");
-// const { describe } = require("yargs");
-// const { beforeAll } = require("@jest/globals");
-
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 // Note - the following codes is same for all HMTL code being loaded to the dom.
 beforeAll(() => {
@@ -17,6 +12,8 @@ beforeAll(() => {
     document.write(fileContents);
     document.close();
 });
+
+// --------------------------------------------------- Const tests
 
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
@@ -36,6 +33,8 @@ describe("game object contains correct keys", () => {
     });
 });
 
+// --------------------------------------------------- Function tests
+
 describe("newGame works correctly", () => {
     beforeAll(() => {
         game.score = 42;
@@ -47,13 +46,38 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear currentGame array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should clear playerMoves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
+    });
+});
+
+// --------------------------------------------------- Gameplay tests
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");
     });
 });
